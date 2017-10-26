a956a5091cd104185209a13837149ee4d5af056c
Jan Helge Aalbu <jhaalbu@gmail.com>
2017-10-26 21:54:57 +0200

    'grafæ

--- s1.py
+++ s1.py
@@ -5,37 +5,45 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def graph(chartID = 'chart_ID', chart_type = 'pie', chart_height = 500):
+from wtforms import Form, IntegerField, StringField, validators
+
+class InputForm(Form):
+    veg = StringField(label='Veg (f.eks fv60)', validators=[validators.InputRequired()])
+    fylke = IntegerField(label='Fylke (f.eks 14)', validators=[validators.InputRequired()])
+
+def hentSkred(veg, fylke):
+    skredtypenr = [4198, 4199, 5351, 4200, 4201, 4202, 4203, 13103]
+    skredtype = {4198 : 'Stein', 4199: 'Jord/løsmasse', 5351: 'Is/stein', 4200: 'Snø', 4201: 'Is', 4202: 'Flomskred (vann+stein+jord)', 4203: 'Sørpeskred (vann+snø+stein)', 13103: 'Utglidning av veg'}
+    liste = []
+    skred = nvdbapi.nvdbFagdata(445)
+    skred.addfilter_geo( { 'vegreferanse' : veg, 'fylke' : fylke})
+    sNum = 0
+    data = []
+    for i in skredtypenr:
+        print(i)
+        print(skredtype[i])
+        skred.addfilter_egenskap('2326=' + str(skredtypenr[sNum]))
+        stat = skred.statistikk()
+        data.append([skredtype[i], stat['antall']])
+        sNum = sNum + 1
+    return data
+
+@app.route('/', methods=['GET', 'POST'])
+def index(chartID = 'chart_ID', chart_type = 'pie', chart_height = 500):
+	form = InputForm(request.form)
+	if request.method == 'POST' and form.validate():
+		data = hentSkred(form.veg.data, form.fylke.data)
+	else:
+		data = None
+
 	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
-	series = [{"name": 'Label1', "data": [['foo',2],['bar',4],['monty',5]]}]
-	title = {"text": 'My Title'}
+	series = [{"name": 'Antall skred', "data": data}] #legg til data variabel istaden for liste i liste
+	title = {"text": 'Antall skred fordelt på type'}
 	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
 	yAxis = {"title": {"text": 'yAxis Label'}}
-	return render_template('index.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
-# print(data)
-#skred = nvdbapi.nvdbFagdata(445)
-#skred.addfilter_geo( { 'vegreferanse' : 'Fv241', 'fylke' : 14})
-#skred.addfilter_egenskap('2326=4198')
+	return render_template('graf.html', form=form, chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
-
-#print(skred.statistikk())
-#ettskred = skred.nesteNvdbFagObjekt()
-#print(ettskred.egenskapverdi(2326))
-#print(ettskred.egenskapverdi(2324))
-#print(ettskred.egenskap(2326))
-"""
-i = 0
-for i in range(0, 1):
-    i = i + 1
-    ettskred = skred.nesteNvdbFagObjekt()
-    print(ettskred.egenskapverdi(2326))
-    print(ettskred.egenskapverdi(2324))
-    print(ettskred.egenskapverdi(7905))
-    print(ettskred.wkt())
-    print(ettskred.egenskapverdi(2326))
-"""
 
 if __name__ == "__main__":
 	app.run()
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pandas as pd
 from flask import Flask, request, render_template
 from wtforms import Form, IntegerField, StringField, validators
 import requests
-
+"""
 def getSkred(veg, fylke):
     skredtype = {4198 : 'Stein', 4199: 'Jord/løsmasse', 5351: 'Is/stein', 4200: 'Snø', 4201: 'Is', 4202: 'Flomskred (vann+stein+jord)', 4203: 'Sørpeskred (vann+snø+stein)', 13103: 'Utglidning av veg'}
     s = nvdbapi.nvdbFagdata(445)
@@ -14,22 +14,38 @@ def getSkred(veg, fylke):
     for i in skredtype[i][0]:
         antallskred[skredtype[]]
         i = i + 1
-
+"""
 
 #print(data)
+def hentSkred(veg='Fv241', fylke=14):
+    skredtypenr = [4198, 4199, 5351, 4200, 4201, 4202, 4203, 13103]
+    skredtype = {4198 : 'Stein', 4199: 'Jord/løsmasse', 5351: 'Is/stein', 4200: 'Snø', 4201: 'Is', 4202: 'Flomskred (vann+stein+jord)', 4203: 'Sørpeskred (vann+snø+stein)', 13103: 'Utglidning av veg'}
+    liste = []
+    skred = nvdbapi.nvdbFagdata(445)
+    skred.addfilter_geo( { 'vegreferanse' : veg, 'fylke' : fylke})
+#print(skred.egenskaper())
+#skred.addfilter_egenskap('2326=4198')
+#stat = skred.statistikk()
+#liste.append((skredtype[skredtypenr[0]], stat['antall']))
+    sNum = 0
+    data = []
+    for i in skredtypenr:
+        print(i)
+        print(skredtype[i])
+        skred.addfilter_egenskap('2326=' + str(skredtypenr[sNum]))
+        stat = skred.statistikk()
+        data.append([skredtype[i], stat['antall']])
+        sNum = sNum + 1
+    return data
 
+print(hentSkred())
 
-skred = nvdbapi.nvdbFagdata(445)
-skred.addfilter_geo( { 'vegreferanse' : 'Fv241', 'fylke' : 14})
-print(skred.egenskaper())
-skred.addfilter_egenskap('2326=4198')
-
-
-print(skred.statistikk())
-ettskred = skred.nesteNvdbFagObjekt()
-print(ettskred.egenskapverdi(2326))
-print(ettskred.egenskapverdi(2324))
-print(ettskred.egenskap(2326))
+#print(liste)
+#print(skred.statistikk())
+#ettskred = skred.nesteNvdbFagObjekt()
+#print(ettskred.egenskapverdi(2326))
+#print(ettskred.egenskapverdi(2324))
+#print(ettskred.egenskap(2326))
 """
 i = 0
 for i in range(0, 1):
